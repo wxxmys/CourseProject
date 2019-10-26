716646d464f2c3ce33ace67a3cc4ce210a22047e
1178154899 <1178154899@qq.com>
2019-10-26 11:26:06 +0800

    添加项目文件。

---
+++ DragonBoatClassroom/EntityDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DragonBoatClassroom
+{
+    public class EntityDbContext:DbContext
+    {
+        public EntityDbContext() { }
+        public EntityDbContext(DbContextOptions options):base(options) { }
+      
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            string connetionString = configuration["ConnectionStrings:Default"];
+            optionsBuilder.UseSqlServer(connetionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
